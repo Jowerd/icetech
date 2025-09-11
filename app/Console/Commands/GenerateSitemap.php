379e0fe8b17dfd9ea517a533af71b3928317e3fa
Jowerd <n.kapanadze002@gmail.com>
2025-09-11 21:52:01 +0400

    update

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -8,7 +8,7 @@ use Spatie\Sitemap\Tags\Url;
 
 use App\Models\Category;
 use App\Models\Product;
-use App\Models\BlogPost; // <-- შეიცვალა Post-ის ნაცვლად
+use App\Models\BlogPost;
 
 class GenerateSitemap extends Command
 {
@@ -22,7 +22,7 @@ class GenerateSitemap extends Command
         $sitemap = SitemapGenerator::create(config('app.url'))
             ->getSitemap();
         
-        // სტატიკური გვერდები
+        // დატოვეთ მხოლოდ ეს ბლოკი, რომელიც შეიცავს სრულ ინფორმაციას
         $sitemap->add(Url::create('/')->setPriority(1.0)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
         $sitemap->add(Url::create('/products')->setPriority(0.8)->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY));
         $sitemap->add(Url::create('/about')->setPriority(0.8)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
@@ -38,7 +38,6 @@ class GenerateSitemap extends Command
             $sitemap->add(Url::create("/products/{$product->slug}")->setPriority(0.8)->setLastModificationDate($product->updated_at));
         });
 
-        // <-- შეიცვალა Post-ის ნაცვლად
         BlogPost::all()->each(function (BlogPost $post) use ($sitemap) {
             $sitemap->add(Url::create("/blog/{$post->slug}")->setPriority(0.8)->setLastModificationDate($post->updated_at));
         });
